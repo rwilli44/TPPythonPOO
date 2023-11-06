55538de0ca282ee28b5bfafc9f7d2cb57c8258c9
Rachel Williams <rachelwilliams@mailbox.org>
2023-11-06 10:39:53 +0100

    Corrected errors in test function Exo-1

--- Exercice-1/entreprise.py
+++ Exercice-1/entreprise.py
@@ -7,23 +7,6 @@ class Entreprise:
     def __str__(self):
         return f"L'entreprise {self.nom}, ayant son siège social au {self.adresse}, possède le numéro de SIRET {self.__siret}"
         
-        
-    # Seul le siret a besoin de vérification, donc je n'ai pas fait d'autres getters/setters
-    @property
-    def siret(self):
-        return self.__numero_SIRET
-    
-    @siret.setter
-    def siret(self, nouveau_siret: str):
-        nouveau_siret = str(nouveau_siret)
-        # Si le nouveau numéro est conforme, le mise à jour est fait
-        try:
-            if self.verify_siret(nouveau_siret):
-                self.__siret = nouveau_siret
-        except ValueError as e:
-            print(e) # en cas d'erreur un message s'affiche mais le programme ne plante pas, l'ancient siret est gardé
-        
-            
     def verify_siret(self,nouveau_siret: str) -> bool:
         """Vérifier que le numero SIRET est de 14 chiffres. 
            Raise ValueError avec message précis le cas échéant
@@ -32,15 +15,28 @@ class Entreprise:
             nouveau_siret (str): le nouveau numéro siret de 14 chiffres
 
         Raises:
-            ValueError: si le string n'est pas composé de 14 chiffres, 
-            un message d'erreur précis s'affiche
+            ValueError: Le numéro SIRET doit être de 14 chiffres
 
         Returns:
-            str: Si valide le nouvea siret est retourné
+            str: Si valide le nouveau siret est retourné
         """
         if nouveau_siret.isdigit() and (len(nouveau_siret) == 14):
             return nouveau_siret
         else:
             raise ValueError("ERREUR : Le numéro SIRET doit être de 14 chiffres.")
             
+    @property
+    def siret(self):
+        return self.__numero_SIRET
+    
+    @siret.setter
+    def siret(self, nouveau_siret: str):
+        try:
+            if self.verify_siret(nouveau_siret): # vérifier que le format est valide
+                self.__siret = nouveau_siret
+        except ValueError as e:
+            print(e) # en cas d'erreur un message s'affiche et l'ancien siret est gardé
+        
+            
+    
             
--- Exercice-1/main.py
+++ Exercice-1/main.py
@@ -1,6 +1,7 @@
 from entreprise import Entreprise
 
 def main():
+    """ Cette fonction teste la classe Entreprise"""
     
     # Créer un objet Entreprise pour les testes
     test_entreprise = Entreprise("Test et Cie.", "55 Rue du Faubourg Saint-Honoré, 75008 Paris", "12345678901214")
@@ -9,20 +10,19 @@ def main():
     print("\n\n\n  **************** Voici le print de l'objet entreprise **************** \n")
     print(test_entreprise)
     
-    # Print le nom de l'entreprise teste
+    # Print le nom de l'entreprise
     print("\n\n\n  **************** Voici le print du nom de l'entreprise **************** \n")
     print(test_entreprise.nom)
     
-    # Changer le siret via un setter et envoyer un int pour montrer que la Classe a prévu de le changer en string
-    # en cas de besoin
-    test_entreprise.siret = 14120987654321
+    # Changer le siret de l'entreprise
+    test_entreprise.siret = "14120987654321"
 
     # Print l'objet pour vérifier que le siret a changé
     print("\n\n\n  **************** Voici le print de l'objet entreprise avec un nouveau siret **************** \n")
     print(test_entreprise)
     
     #### Tests Bonus ####
-    # Tester la vérification du siret par le setter avec un siret non-conforme
+    # Tester la vérification du siret avec un siret non-conforme
     # Devrait afficher un message d'erreur mais continuer le code
     print("\n\n\n  **************** Voici un print bonus de l'erreur reçu si on utilise un siret non valide - lettre **************** \n")
 
@@ -41,16 +41,11 @@ def main():
     print("\n\n\n  **************** Voici le print de l'objet entreprise pour vérifier que le siret n'a pas changé **************** \n")
     print(test_entreprise)
 
-    # Essayer d'initialiser une instance avec un mauvais siret, l'instance sera créer mais sans siret
-    # Je n'ai pas trouvé de moyen de ne pas créer l'objet de tout 
+    # Essayer d'initialiser une instance avec un mauvais siret, le programme plante pour ne pas créer un objet
+    # qui n'est pas valide, mais affiche le message d'erreur en fin du Traceback
     print("\n\n\n  ***************** Une tentative de créer un objet avec un mauvais siret devrait provoquer une erreur **************** \n **************** et terminer le programme avec un message après le TraceBack pour expliquer le problème  **************** \n")
 
-    bad_entreprise = Entreprise("Unreal Entreprise ","123 Bad Street","NoGood123")
-    
-    print("\n\n\n  **************** Voici le print de l'objet sans siret qui devrait provoquer une **************** \n **************** erreur et terminer le programme car il n'a pas de siret **************** \n")
-
-    print(bad_entreprise)
-    
+    test_mauvais_siret = Entreprise("Unreal Entreprise ","123 Bad Street","NoGood123")
     
         
 main()
