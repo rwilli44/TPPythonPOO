631801b92d89b0b9d834d1c713497e346d233b1f
Rachel Williams <rachelwilliams@mailbox.org>
2023-11-06 20:32:32 +0100

    Add readme and update exo1 to comment out test that crashes the program

--- Exercice-1/main.py
+++ Exercice-1/main.py
@@ -6,7 +6,7 @@ def main():
     # Créer un objet Entreprise pour les testes
     test_entreprise = Entreprise("Test et Cie.", "55 Rue du Faubourg Saint-Honoré, 75008 Paris", "12345678901214")
     
-    # Print l'objet teste pour vérifier son __str__
+    # Print l'objet pour vérifier son __str__
     print("\n\n\n  **************** Voici le print de l'objet entreprise **************** \n")
     print(test_entreprise)
     
@@ -43,9 +43,9 @@ def main():
 
     # Essayer d'initialiser une instance avec un mauvais siret, le programme plante pour ne pas créer un objet
     # qui n'est pas valide, mais affiche le message d'erreur en fin du Traceback
-    print("\n\n\n  ***************** Une tentative de créer un objet avec un mauvais siret devrait provoquer une erreur **************** \n **************** et terminer le programme avec un message après le TraceBack pour expliquer le problème  **************** \n")
+    # print("\n\n\n  ***************** Une tentative de créer un objet avec un mauvais siret devrait provoquer une erreur **************** \n **************** et terminer le programme avec un message après le TraceBack pour expliquer le problème  **************** \n")
 
-    test_mauvais_siret = Entreprise("Unreal Entreprise ","123 Bad Street","NoGood123")
+    # test_mauvais_siret = Entreprise("Unreal Entreprise ","123 Bad Street","NoGood123")
     
         
 main()
---
+++ readme.md
@@ -0,0 +1,15 @@
+# TP Python POO - Rachel WILLIAMS 11/06/2023
+
+Merci beaucoup Robin pour ta disponibilité et ton écoute. J'ai pris beaucoup de plaisir à travailler le RPG et à compléter ce TP. Voici quelques précisions sur mon travail.
+
+Pour chaque exercice j'ai créé un fichier **main.py** pour la côté test ou interaction.
+
+## Exercice 1
+
+J'ai ajouté quelques tests supplémentaires pour montrer comment l'erreur est géré au cas où l'utilisateur tente de changer le numéro SIRET pour un numéro qui n'est pas valide. Si l'utilisateur essaie de créer un objet Entreprise avec un mauvais numéro au départ, le programme plante pour éviter de créer un objet sans numéro SIRET. Le dernier test qui vérifie ça est commenté.
+
+## Exercice 2
+
+## Exercice 3
+
+## Exercice 4
